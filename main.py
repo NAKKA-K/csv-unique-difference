a152f339c36508e21dc6a9c7c6010f2d4436dd35
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2022-11-29 02:21:17 +0900

    Remove default option

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import csv
 import pprint
 
 with open("./sample.csv", "r") as csv_file:
-    reader = csv.reader(csv_file, delimiter=",")
+    reader = csv.reader(csv_file)
     for row in reader:
         print(row)
